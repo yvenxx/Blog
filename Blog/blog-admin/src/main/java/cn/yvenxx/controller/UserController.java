e700b3071686fe392ba9d849c712a20105d0c935
yven <yvenxx@163.com>
2024-03-18 15:24:33 +0800

    spring security config

--- Blog/blog-admin/src/main/java/cn/yvenxx/controller/UserController.java
+++ Blog/blog-admin/src/main/java/cn/yvenxx/controller/UserController.java
@@ -3,13 +3,15 @@ package cn.yvenxx.controller;
 import cn.yvenxx.R;
 import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Api(tags = "用户管理")
+@RequestMapping("/user")
 public class UserController {
-    @GetMapping("/info")
+    @GetMapping("info")
     public R info(String id){
-        return R.ok(null);
+       return R.ok("asdf");
     }
 }
